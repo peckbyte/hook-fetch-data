b6d5ae13641dc2e5a61bbbda886ccabf58d657c8
peckbyte <503410224@qq.com>
2020-02-06 22:27:40 +0800

    custom data fetch hook
    init custom data fetch hook

--- src/fetchData.tsx
+++ src/fetchData.tsx
@@ -5,7 +5,7 @@ export default function FetchDataApp(props) {
   const [data, setData] = useState<{ hits: any[] }>({ hits: [] });
   const [query, setQuery] = useState("redux");
   const [url, setUrl] = useState(
-    "https://hn.algolia.com/api/v1/search?query=redux"
+    "https://hn.algolia.com/api/v1/search?query=mobx"
   );
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
@@ -27,16 +27,20 @@ export default function FetchDataApp(props) {
 
   return (
     <>
-    <form 
-    onSubmit = {
-       e=>{setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)
-       e.preventDefault() /* but now browser reloading when clicking the submit button,because that's the native behavior of the browser when submitting a form*/
-  }}
-    >
-    <input type="text" value={query} onChange={e => setQuery(e.target.value)} />
-      <button type = 'submit'>search</button>
-    </form>
-      
+      <form
+        onSubmit={e => {
+          setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`);
+          e.preventDefault(); /* but now browser reloading when clicking the submit button,because that's the native behavior of the browser when submitting a form*/
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+        />
+        <button type="submit">search</button>
+      </form>
+
       {isError && <div>something went wrong</div>}
       {isLoading ? (
         <div>loading...</div>
