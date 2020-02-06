5be3ac588706bc56d91b78d925b79c8f598d86ac
peckbyte <503410224@qq.com>
2020-02-06 22:16:11 +0800

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# hook-fetch-data
-Created with CodeSandbox
---
+++ src/fetchData.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function FetchDataApp(props) {
+  const [data, setData] = useState<{ hits: any[] }>({ hits: [] });
+  const [query, setQuery] = useState("redux");
+  const [url, setUrl] = useState(
+    "https://hn.algolia.com/api/v1/search?query=redux"
+  );
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  useEffect(() => {
+    const fetchdata = async () => {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const result = await axios(url);
+        setData(result.data);
+      } catch (e) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+    fetchdata();
+  }, [url]);
+
+  return (
+    <>
+    <form 
+    onSubmit = {
+       e=>{setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)
+       e.preventDefault() /* but now browser reloading when clicking the submit button,because that's the native behavior of the browser when submitting a form*/
+  }}
+    >
+    <input type="text" value={query} onChange={e => setQuery(e.target.value)} />
+      <button type = 'submit'>search</button>
+    </form>
+      
+      {isError && <div>something went wrong</div>}
+      {isLoading ? (
+        <div>loading...</div>
+      ) : (
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
